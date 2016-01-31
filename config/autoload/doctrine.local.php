07299dbc25c37cd74f96881f2009f4c19cd2626d
Fabio <bottanfabio@hotmail.it>
2016-01-31 19:29:57 +0100

    Other views and entities update gitignore

---
+++ config/autoload/doctrine.local.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            'orm_another' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                    'params' => array(
+                        'user' => 'root',
+                        'password' => 'root',
+                ),
+            ),
+        )
+));
